2877eb8f2696448e747f75c2bac9658a6ac72b53
MrGians <gianlumura97@gmail.com>
2022-08-23 15:54:05 +0200

    header static navbar done

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,5 +1,37 @@
 <template>
-  <header></header>
+  <header>
+    <div class="container">
+      <!-- Main Navbar -->
+      <nav id="navbar">
+        <!-- Logo -->
+        <a href="#">
+          <img class="logo" src="./../assets/img/logo-dark.png" alt="Landrick Logo" />
+        </a>
+        <!-- Navigation -->
+        <ul class="navigation">
+          <li><a class="active" href="#">Home</a></li>
+          <li><a href="#">Landing</a></li>
+          <li><a href="#">Pages</a></li>
+          <li><a href="#">Docs</a></li>
+        </ul>
+        <!-- Navigation CTA Icon -->
+        <ul class="navigation-icons">
+          <li>
+            <a href="#"><FaIcon icon="fa-solid fa-magnifying-glass" /></a>
+          </li>
+          <li>
+            <a class="cs-nav-icon" href="#"><FaIcon icon="fa-brands fa-github" /></a>
+          </li>
+          <li>
+            <a class="cs-nav-icon" href="#"><FaIcon icon="fa-brands fa-stack-overflow" /></a>
+          </li>
+          <li>
+            <a class="cs-nav-icon" href="#"><FaIcon icon="fa-solid fa-user-large" /></a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -8,4 +40,58 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "./../assets/styles/style.scss";
+
+header {
+  background-color: $primary-bg-color;
+}
+
+#navbar {
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    height: 35px;
+  }
+
+  .navigation {
+    display: flex;
+    font-weight: 500;
+    margin: 0;
+
+    a {
+      color: $primary-text-color;
+      padding: 0 1rem;
+      text-transform: uppercase;
+
+      &.active {
+        color: $tertiary-text-color;
+      }
+    }
+  }
+  .navigation-icons {
+    display: flex;
+    margin: 0;
+
+    a {
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: $secondary-text-color;
+      padding: 0.5rem;
+      margin: 0 0.3rem;
+
+      &.cs-nav-icon {
+        color: $tertiary-text-color;
+        background-color: $secondary-bg-color;
+        border-radius: 5px;
+        box-shadow: 0px 2px 4px -1px currentColor;
+      }
+    }
+  }
+}
+</style>
