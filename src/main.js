abdd7758db5d42b70b99ae1a9512f0dfef53fce5
MrGians <gianlumura97@gmail.com>
2022-08-23 12:42:17 +0200

    Added FontAwesome Icon

--- src/main.js
+++ src/main.js
@@ -1,8 +1,57 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+
+import { faMagnifyingGlass } from "@fortawesome/free-regular-svg-icons";
+import { faStackOverflow } from "@fortawesome/free-regular-svg-icons";
+import { faGithub } from "@fortawesome/free-regular-svg-icons";
+import { faUserLarge } from "@fortawesome/free-regular-svg-icons";
+
+import { faLayerGroup } from "@fortawesome/free-regular-svg-icons";
+import { faTablet } from "@fortawesome/free-regular-svg-icons";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import { faMaximize } from "@fortawesome/free-regular-svg-icons";
+
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
+
+import { faAngleDown } from "@fortawesome/free-regular-svg-icons";
+import { faAngleRight } from "@fortawesome/free-regular-svg-icons";
+import { faArrowRight } from "@fortawesome/free-regular-svg-icons";
+
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(
+  faGear,
+  faMagnifyingGlass,
+  faStackOverflow,
+  faGithub,
+  faUserLarge,
+  faLayerGroup,
+  faTablet,
+  faPenToSquare,
+  faMaximize,
+  faCircleCheck,
+  faAngleDown,
+  faAngleRight,
+  faArrowRight,
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+  faLinkedinIn,
+  faEnvelope
+);
+Vue.component("FaIcon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
