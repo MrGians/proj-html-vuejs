74be5b8527088035b73bedf4b2585a031b13820c
MrGians <gianlumura97@gmail.com>
2022-08-23 18:53:58 +0200

    Added Main component

---
+++ src/components/layout/BaseMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <!-- Key Features Section -->
+    <!-- Quickstart Section -->
+    <!-- Build tools Section -->
+    <!-- Business Plan Section -->
+    <!-- Find out Section -->
+  </main>
+</template>
+
+<script>
+export default {
+  name: "BaseMain",
+};
+</script>
+
+<style lang="scss" scoped></style>
