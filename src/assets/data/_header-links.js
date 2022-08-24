32c4a72c81769ff8b5a3a1e6d23f3b697496d96b
MrGians <gianlumura97@gmail.com>
2022-08-24 12:50:42 +0200

    Fix header links data

--- src/assets/data/_header-links.js
+++ src/assets/data/_header-links.js
@@ -1,22 +1,54 @@
-export default [
-  {
-    text: "Home",
-    url: "#",
-    current: true,
-  },
-  {
-    text: "Landing",
-    url: "#",
-    current: false,
-  },
-  {
-    text: "Pages",
-    url: "#",
-    current: false,
-  },
-  {
-    text: "Docs",
-    url: "#",
-    current: false,
-  },
-];
+export default {
+  navLinks: [
+    {
+      id: 1,
+      text: "Home",
+      url: "#",
+      current: true,
+    },
+    {
+      id: 2,
+      text: "Landing",
+      url: "#",
+      current: false,
+    },
+    {
+      id: 3,
+      text: "Pages",
+      url: "#",
+      current: false,
+    },
+    {
+      id: 4,
+      text: "Docs",
+      url: "#",
+      current: false,
+    },
+  ],
+  navIcons: [
+    {
+      id: 1,
+      name: "Search",
+      url: "#",
+      icon: "fa-solid fa-magnifying-glass",
+    },
+    {
+      id: 2,
+      name: "Github",
+      url: "#",
+      icon: "fa-brands fa-github",
+    },
+    {
+      id: 3,
+      name: "Stack Overflow",
+      url: "#",
+      icon: "fa-brands fa-stack-overflow",
+    },
+    {
+      id: 4,
+      name: "User",
+      url: "#",
+      icon: "fa-solid fa-user-large",
+    },
+  ],
+};
