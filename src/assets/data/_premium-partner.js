277b1409cb3df396c1a399b05f7e19dd9bc01335
MrGians <gianlumura97@gmail.com>
2022-08-24 13:25:02 +0200

    Premium partners data fix

--- src/assets/data/_premium-partner.js
+++ src/assets/data/_premium-partner.js
@@ -1,8 +1,8 @@
 export default [
-  { text: "Amazon", src: require("./../img/amazon.svg") },
-  { text: "Google", src: require("./../img/google.svg") },
-  { text: "Lenovo", src: require("./../img/lenovo.svg") },
-  { text: "Paypal", src: require("./../img/paypal.svg") },
-  { text: "Shopify", src: require("./../img/shopify.svg") },
-  { text: "Spotify", src: require("./../img/spotify.svg") },
+  { text: "Amazon", src: require("./../img/amazon.svg"), id: 1 },
+  { text: "Google", src: require("./../img/google.svg"), id: 2 },
+  { text: "Lenovo", src: require("./../img/lenovo.svg"), id: 3 },
+  { text: "Paypal", src: require("./../img/paypal.svg"), id: 4 },
+  { text: "Shopify", src: require("./../img/shopify.svg"), id: 5 },
+  { text: "Spotify", src: require("./../img/spotify.svg"), id: 6 },
 ];
